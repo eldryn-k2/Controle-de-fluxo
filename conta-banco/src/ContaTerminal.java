6b0bcfbedbde7de190b366afe40fd19a97b7b832
eldryn <eldryn_tw@hotmail.com>
2025-06-16 16:27:46 -0300

    Projeto Dio Terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+ import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor, digite o número da conta:");
+        int numeroConta = scanner.nextInt();
+        
+        System.out.println("Por favor, digite o número da agência:");
+        String numeroAgencia = scanner.next();
+        
+        System.out.println("Por favor, digite o nome do cliente:");
+        String nomeCliente = scanner.next();
+        
+        System.out.println("Por favor, digite o saldo da conta:");
+        double saldoConta = scanner.nextDouble();
+        
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s e o número da sua conta é %d. Seu saldo é de R$ %.2f%n", 
+                          nomeCliente, numeroAgencia, numeroConta, saldoConta);
+        
+        scanner.close();
     }
 }
